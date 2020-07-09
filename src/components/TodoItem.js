cc6c34b3b2e63418666fe224966fef5e7ae13cc0
thamaragerigr <thamaragerigr@gmail.com>
2020-07-09 23:10:49 +0200

    changed app title

---
+++ src/components/TodoItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function TodoItem() {
+  return (
+    <div className="todo-item">
+       <input type="checkbox"/>
+       <p>PlaceHolder</p>
+    </div>
+  );
+}
+
+export default TodoItem;
