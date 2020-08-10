cefe8f40daea998267d488566565173d83987c2e
thamaragerigr <thamaragerigr@gmail.com>
2020-08-10 11:37:40 +0200

    Updates

---
+++ precache-manifest.458ebb4dbcb7b0b23496dd7b800b564a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fb4978a7d446083896eb38eafaf3fe8",
+    "url": "/Card-Picker/index.html"
+  },
+  {
+    "revision": "1253e56aad4646d93038",
+    "url": "/Card-Picker/static/css/main.8529d7ea.chunk.css"
+  },
+  {
+    "revision": "875205d976cef2a3b422",
+    "url": "/Card-Picker/static/js/2.0bde1dd1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Card-Picker/static/js/2.0bde1dd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1253e56aad4646d93038",
+    "url": "/Card-Picker/static/js/main.d40ae467.chunk.js"
+  },
+  {
+    "revision": "2107022039c2a165a050",
+    "url": "/Card-Picker/static/js/runtime-main.d391fa58.js"
+  }
+]);
